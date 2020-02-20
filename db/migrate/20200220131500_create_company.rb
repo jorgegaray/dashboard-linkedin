0ef142c565b2c5c9b11be22ddaba16579e04076c
Jorge Garay Chandia <jgaray@buk.cl>
2020-02-20 10:37:14 -0300

    feat: agregar modelo company

---
+++ db/migrate/20200220131500_create_company.rb
@@ -0,0 +1,14 @@
+class CreateCompany < ActiveRecord::Migration[5.1]
+  def change
+    create_table :companies do |t|
+      t.string :name, null: false
+      t.string :description
+      t.string :area
+      t.string :web
+      t.integer :linkeind_id
+      t.integer :country, default: 0
+      t.integer :size
+      t.string :email
+    end
+  end
+end
