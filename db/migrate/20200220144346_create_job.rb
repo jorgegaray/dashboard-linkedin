b1ffb507129e3064d04bf84c9779ef7b92ca9c1e
Felipe Salgado <felipe.salgado.j@ing.uchile.cl>
2020-02-20 13:46:41 -0300

    fix: ids extra en relacion

--- db/migrate/20200220144346_create_job.rb
+++ db/migrate/20200220144346_create_job.rb
@@ -6,10 +6,6 @@ class CreateJob < ActiveRecord::Migration[5.1]
       t.string :role
       t.date :start_date
       t.date :end_date
-      t.integer :company_id, null: false
-      t.integer :person_id, null: false
     end
-
-    add_reference :companies, :job, foreign_key: true, index: true
   end
 end
